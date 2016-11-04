35e1d6e3a895212b9df4afe6164ad0c3db53a35c
Edgar <ed.ruiz1106@gmail.com>
2016-11-04 03:38:56 +0000

    Add devise for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103215545) do
+ActiveRecord::Schema.define(version: 20161104031450) do
 
   create_table "movies", force: :cascade do |t|
-    t.string   "movie_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "movie_title"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "movie_director"
+    t.string   "movie_runtime"
+    t.string   "movie_genre"
+    t.string   "movie_rated"
+    t.string   "movie_plot"
+    t.string   "movie_poster"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
